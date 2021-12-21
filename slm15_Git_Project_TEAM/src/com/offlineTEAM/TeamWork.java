06a4f5148f762101b393247bc6b77143f8dee45b
Gauravnasurde <96283099+Gauravnasurde@users.noreply.github.com>
2021-12-21 11:17:35 +0530

    Update TeamWork.java
    One Method added

--- slm15_Git_Project_TEAM/src/com/offlineTEAM/TeamWork.java
+++ slm15_Git_Project_TEAM/src/com/offlineTEAM/TeamWork.java
@@ -1,13 +1,22 @@
 package com.offlineTEAM;
 
 public class TeamWork {
+	public void m2()
+	{
+		System.out.println("Method By Gaurav")
+	}
 	
 	public static void main(String[] args) {
+		
+		Teamwork yt=new Teamwork();
+		    
+		         yt.m2();
 		
 		public void method() 
 		{
 			System.out.println("hiii--------");
 		}
+		
 	}
 
 }
