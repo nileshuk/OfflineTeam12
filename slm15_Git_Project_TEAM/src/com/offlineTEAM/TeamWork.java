1892e95b51019005cab50f1fe53264fc8c2adba5
nileshuk <96296631+nileshuk@users.noreply.github.com>
2021-12-21 11:20:26 +0530

    add 1method and print deta

--- slm15_Git_Project_TEAM/src/com/offlineTEAM/TeamWork.java
+++ slm15_Git_Project_TEAM/src/com/offlineTEAM/TeamWork.java
@@ -3,10 +3,18 @@ package com.offlineTEAM;
 public class TeamWork {
 	
 	public static void main(String[] args) {
-	
-	
+		
+		public void method() 
+		{
 			System.out.println("hiii--------");
-	
+		}
+		
+		
+		public void Add()
+		{
+			System.out.println("addition of two no");
+		
+		}
 	}
 
 }
