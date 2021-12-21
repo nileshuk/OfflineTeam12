b056601723848ec08fe0caec2ce7c2f4a902e39e
Piyush <96284657+lpiyush@users.noreply.github.com>
2021-12-21 11:17:49 +0530

    Update TeamWork.java

--- slm15_Git_Project_TEAM/src/com/offlineTEAM/TeamWork.java
+++ slm15_Git_Project_TEAM/src/com/offlineTEAM/TeamWork.java
@@ -3,11 +3,10 @@ package com.offlineTEAM;
 public class TeamWork {
 	
 	public static void main(String[] args) {
-		
-		public void method() 
-		{
+	
+	
 			System.out.println("hiii--------");
-		}
+	
 	}
 
 }
